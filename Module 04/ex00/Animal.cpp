18d602ae3315dfd2c614ae6b8d099f4e63e6006d
jchuah <jchuah@student.42kl.edu.my>
2026-01-19 15:39:22 +0800

    module 04 ex00 done

---
+++ Module 04/ex00/Animal.cpp
@@ -0,0 +1,42 @@
+#include "Animal.hpp"
+#include <iostream>
+
+Animal::Animal(void)
+	:_type("meta")
+{
+	std::cout << "Animal default constructor called" << std::endl;
+}
+
+Animal::Animal(std::string type)
+	:_type(type)
+{
+	std::cout << "Animal(type) constructor called" << std::endl;
+}
+
+Animal::Animal(const Animal& other)
+	:_type(other._type)
+{
+	std::cout << "Animal copy constructor called" << std::endl;
+}
+
+Animal&	Animal::operator=(const Animal& other)
+{
+	if (this != &other)
+	{
+		this->_type = other._type;
+	}
+	return (*this);
+	std::cout << "Animal copy assignment operator called" << std::endl;
+}
+
+Animal::~Animal(void)
+{
+	std::cout << "Animal destructor called" << std::endl;
+}
+
+const std::string&	Animal::getType(void) const { return (_type); }
+
+void	Animal::makeSound(void) const
+{
+	std::cout << "\e[3mAnimal noises\e[0m" << std::endl;
+}
---
+++ Module 04/ex00/Dog.cpp
@@ -0,0 +1,34 @@
+#include "Dog.hpp"
+#include <iostream>
+
+Dog::Dog(void)
+	:Animal("Dog")
+{
+	std::cout << "Dog default constructor called" << std::endl;
+}
+
+Dog::Dog(const Dog& other)
+	:Animal(other)
+{
+	std::cout << "Dog copy constructor called" << std::endl;
+}
+
+Dog&	Dog::operator=(const Dog& other)
+{
+	if (this != &other)
+	{
+		Animal::operator=(other);
+	}
+	return (*this);
+	std::cout << "Dog copy assignment operator called" << std::endl;
+}
+
+Dog::~Dog(void)
+{
+	std::cout << "Dog destructor called" << std::endl;
+}
+
+void	Dog::makeSound(void) const
+{
+	std::cout << "\e[3mWoof Woof\e[0m" << std::endl;
+}
